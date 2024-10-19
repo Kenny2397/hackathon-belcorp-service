e5be0fd0c379cb9f3df5b720cd59a4800379f03d
kenny <kenny.luque.t@uni.pe>
2024-10-19 01:14:15 -0500

    ci(deploy): Add ci/cd

---
+++ src/functions/product-recomendation.ts
@@ -0,0 +1,21 @@
+import { ProductRecomendationController } from '@infrastructure/adapters/in/http/GetAllPostController'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+import { logger } from 'src/powertools/utilities'
+
+const productRecomendationController = new ProductRecomendationController(
+
+)
+
+export const handler = async (event: APIGatewayProxyEvent, _context: Partial<Context>)
+: Promise<APIGatewayProxyResult | unknown> => {
+
+  logger.logEventIfEnabled(event)
+
+  const res = await productRecomendationController.exec(event)
+
+  return res
+}
+
+
+
+
